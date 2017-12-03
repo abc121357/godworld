e196249d3ba8984c470c8943c735ca1ec82182de
abc121357 <abc121357@naver.com>
2017-12-04 08:55:55 +0900

    abc

---
+++ Documents/moving/birdtravel/Assets/Scripts/moveS21.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveS21 : MonoBehaviour
+{
+    int a = 1;
+    public float speed = 5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.localPosition.y > 1.85f)
+        {
+            a = -1;
+        }
+        else if (transform.localPosition.y < -2.85f)
+        {
+            a = 1;
+        }
+
+
+        transform.Translate(Vector2.up * speed * Time.deltaTime * a);
+
+
+    }
+}
