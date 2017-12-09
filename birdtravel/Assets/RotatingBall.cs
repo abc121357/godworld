75af784289573506246cec14206b450e187be0a6
sesang <sesang06@naver.com>
2017-12-10 01:04:05 +0900

    움직이는 돌덩어리
    생성

---
+++ birdtravel/Assets/RotatingBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBall : MonoBehaviour {
+    public float speed = 5f;
+    public float pivotOffsetX = 0f;
+    public float pivotOffsetY = 0f;
+    private Vector3 pivot;
+   // public GameObject Ball;
+    // Use this for initialization
+    void Start () {
+        pivot = new Vector3();
+        pivot.x = transform.position.x + pivotOffsetX;
+        pivot.y = transform.position.y + pivotOffsetY;
+        pivot.z = transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update (){
+        
+        float angle = Mathf.Atan2(pivot.x, pivot.y) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        
+      //  transform.Rotate( Vector3.forward, Time.deltaTime * speed);
+        transform.RotateAround(pivot, Vector3.forward, Time.deltaTime * speed);
+	}
+}
