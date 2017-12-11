529cdde2d25d13dc6127cee187437a5cbcd4b6bf
sesang <sesang06@naver.com>
2017-12-12 00:11:12 +0900

    move04 관련 버그 수정
    start, end 대소관계 이제 신경 안 써도 됩니다.

--- birdtravel/Assets/Scripts/GameObjects/move04.cs
+++ birdtravel/Assets/Scripts/GameObjects/move04.cs
@@ -4,30 +4,45 @@ using UnityEngine;
 
 
 public class move04 : MonoBehaviour {
-	public int direction = 1;
+	private Vector2 direction = Vector2.up;
     public float speed = 5;
     // Use this for initialization
 
     public float endPoint; // = 4.85f;
     public float startPoint; // -0.85f
 
+    private float minPoint; //최소값
+    private float maxPoint; // 최대값
     void Start () {
+        if (endPoint > startPoint)
+        {
+            maxPoint = endPoint;
+            minPoint = startPoint;
+        }
+        else
+        {
+            minPoint = endPoint;
+            maxPoint = startPoint;
+        }
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (transform.localPosition.y > endPoint) {
-			direction = -1;
+		if (transform.localPosition.y < minPoint) {
+
+            
+        
+            direction = Vector2.up;
 		}
-		else if (transform.localPosition.y < startPoint) {
-			direction = 1;
+		else if (transform.localPosition.y > maxPoint) {
+         
+            direction = Vector2.down;
 		}
+        transform.Translate(speed * Time.deltaTime * direction);
 
 
-		transform.Translate(Vector2.up*speed*Time.deltaTime* direction);
 
-
-	}
+    }
 }
