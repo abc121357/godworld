d691fd87c8e9ea799a8b33a52bbe709be928bb0c
Jo Sesang <sesang06@naver.com>
2018-01-26 00:05:32 +0900

    트램펄린의 추가
    트램펄린의 추가

---
+++ birdtravel/Assets/Scripts/GameObjects/Trampoline.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour {
+    public float jumpPower = 20.0f;
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            Rigidbody2D rigid = collision.GetComponent<Rigidbody2D>();
+            rigid.velocity = Vector2.zero;
+
+            Vector2 jumpVelocity = new Vector2(0, jumpPower);
+            rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+            movement movement = collision.GetComponent<movement>();
+            if (movement != null)
+            {
+
+
+                //movement.isjumping = true;
+                movement.animator.SetBool("isjumping", true);
+                movement.animator.SetTrigger("dojumping");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
